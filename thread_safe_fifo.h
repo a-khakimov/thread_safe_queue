0e453611ee4b05f2d4c5c1cffc476900e0842e58
Ainur Hakimov <33376759+a-khakimov@users.noreply.github.com>
2020-02-15 19:59:32 +0500

    fix tabs

--- thread_safe_fifo.h
+++ thread_safe_fifo.h
@@ -17,12 +17,12 @@ struct thread_safe_fifo_t;
 extern "C" {
 #endif
 
-int 	thread_safe_fifo_init		(struct thread_safe_fifo_t**				);
-int 	thread_safe_fifo_destroy	(struct thread_safe_fifo_t**				);
-void 	thread_safe_fifo_try_push	(struct thread_safe_fifo_t*, const uint8_t	);
-uint8_t thread_safe_fifo_try_pop	(struct thread_safe_fifo_t*					);
-size_t 	thread_safe_fifo_size		(struct thread_safe_fifo_t* 				);
-bool 	thread_safe_fifo_empty		(struct thread_safe_fifo_t*					);
+int     thread_safe_fifo_init     (struct thread_safe_fifo_t**               );
+int     thread_safe_fifo_destroy  (struct thread_safe_fifo_t**               );
+void    thread_safe_fifo_try_push (struct thread_safe_fifo_t*, const uint8_t );
+uint8_t thread_safe_fifo_try_pop  (struct thread_safe_fifo_t*                );
+size_t  thread_safe_fifo_size     (struct thread_safe_fifo_t*                );
+bool    thread_safe_fifo_empty    (struct thread_safe_fifo_t*                );
 
 #ifdef __cplusplus
 }
